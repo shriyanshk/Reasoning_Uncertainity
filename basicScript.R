4f414e42e4c1bc9f338461df989b7bd9e357d47b
Shriyansh Kothari <shriyanshk@yahoo.com>
2017-04-06 01:20:50 -0600

    Update basicScript.R

--- basicScript.R
+++ basicScript.R
@@ -9,3 +9,6 @@ DEM@crs
 area <- readOGR("Shape File")
 area
 print(coords(area))
+
+#http://stackoverflow.com/questions/5480005/how-can-i-simply-load-a-greyscale-tiff-in-libtiff-and-get-an-array-of-pixel-inte
+#https://www.rforge.net/doc/packages/tiff/readTIFF.html
