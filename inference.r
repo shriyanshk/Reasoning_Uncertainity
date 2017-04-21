0fc813da616fa8d6fd4f72ee1ff0ccf23dc59953
Janette Rounds <janette.rounds@msu.montana.edu>
2017-04-21 15:45:44 -0600

    inference modifications

--- inference.r
+++ inference.r
@@ -51,22 +51,23 @@ library(pcalg)
 #library(RandomFieldsUtils)
 #library(pcalg)
 
-fileVariable <- read.csv(file="C:/Users/shriy/Downloads/drive-download-20170420T000155Z-001/LIDARsub1-2.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+fileVariable <- read.csv(file="C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARsub3NoMissing1Date.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 #set.seed(50)
-numberOfRows <- nrow(fileVariable[3:50])
-labelName <- colnames(fileVariable[3:50])
+numberOfRows <- nrow(fileVariable[3:29])
+labelName <- colnames(fileVariable[3:29])
 
 #Normality
-test <- apply(fileVariable[3:50], 2, shapiro.test)
+test <- apply(fileVariable[3:29], 2, shapiro.test)
 print(sapply(test, function(x) unlist(x[c("statistic", "p.value")])))
 
-  x <- as.data.frame(matrix(fileVariable[3:50] ,ncol = length(labelName)))
-  suffStat <- list(C = cor(fileVariable[3:50]), n = numberOfRows)
+  x <- as.data.frame(matrix(fileVariable[3:29] ,ncol = length(labelName)))
+  suffStat <- list(C = cor(fileVariable[3:29]), n = numberOfRows)
   #pc.fit <- pc(suffStat, indepTest = gaussCItest, alpha = 0.01, labels = labelName, verbose = TRUE)
-  skel.fit <- skeleton(suffStat, indepTest = gaussCItest, p = ncol(fileVariable[3:50]), labels = labelName, alpha = 0.01)
-  if (require(Rgraphviz)) {
+  skel.fit <- skeleton(suffStat, indepTest = gaussCItest, p = ncol(fileVariable[3:29]), labels = labelName, alpha = 0.01)
+  #if (require(Rgraphviz)) {
+  skel.fit
     par(mfrow=c(1,1))
     #plot(fileVariable$RBR.20131117, main = "True DAG")
     plot(skel.fit, main = "Estimated CPDAG")
     
-  }
+ # }
