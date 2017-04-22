9c7adfd2c04cb5e56228e2e64062eaa50831cdce
Janette Rounds <janette.rounds@msu.montana.edu>
2017-04-22 05:54:38 -0600

    non-working CRF inference example

--- discretize.R
+++ discretize.R
@@ -1,16 +1,16 @@
 library(arules)
 #read csv
-lidavdata <- read.csv(file="C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARsub3NoMissing1Date.csv", 
+lidavdata <- read.csv(file="C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARNoMissingBefore.csv", 
                       header = TRUE, sep = ",")
 newdata = lidavdata[,3:29]
 #discretize
 for(i in colnames(newdata)){
 #find cutpoints
-  binneddata = discretize(newdata[,i], method="cluster", categories=3, onlycuts=TRUE)
+  binneddata = discretize(newdata[,i], method="cluster", categories=5, onlycuts=TRUE)
 
   binneddata
 
-  cuts <- cut(x=newdata[,i], breaks=binneddata, labels=1:3, include.lowest=TRUE)
+  cuts <- cut(x=newdata[,i], breaks=binneddata, labels=1:5, include.lowest=TRUE)
 
 
   newdata[,i] = cuts
@@ -20,6 +20,6 @@ for(i in colnames(newdata)){
 
 
 #write to csv
-write.table(newdata, file = "C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARsub1NoMissingDiscretized.csv", 
+write.table(newdata, file = "C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARNoMissingBeforeDiscretized.csv", 
             sep = ",", row.names=FALSE, qmethod = "double")
 
--- inference.r
+++ inference.r
@@ -5,25 +5,42 @@ biocLite("RBGL")
 biocLite('Rgraphviz')
 library(RandomFieldsUtils)
 library(pcalg)
-#fileVariable <- read.csv(file="C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARsub1NoMissingDiscretized.csv", header = TRUE, sep = ",")
+library(CRF)
+fileVariable <- read.csv(file="C:/Users/Jani/Documents/Reasoning/ProjectSources/data/LidarData/LIDARsub1NoMissingDiscretized.csv", header = TRUE, sep = ",")
 
-fileVariable <- read.csv(file="C:/Users/shriy/Downloads/drive-download-20170420T000155Z-001/LIDARsub1-2.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+#fileVariable <- read.csv(file="C:/Users/shriy/Downloads/drive-download-20170420T000155Z-001/LIDARsub1-2.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 #set.seed(50)
-numberOfRows <- nrow(fileVariable[3:50])
-labelName <- colnames(fileVariable[3:50])
+numberOfRows <- nrow(fileVariable)
+labelName <- colnames(fileVariable)
 #Normality
-test <- apply(fileVariable[3:50], 2, shapiro.test)
+test <- apply(fileVariable, 2, shapiro.test)
 #prints Normality
 print(sapply(test, function(x) unlist(x[c("statistic", "p.value")])))
 #converts the csv values to data frame
-x <- as.data.frame(matrix(fileVariable[3:50] ,ncol = length(labelName)))
-suffStat <- list(C = cor(fileVariable[3:50]), n = numberOfRows)
+x <- as.data.frame(matrix(fileVariable ,ncol = length(labelName)))
+suffStat <- list(C = cor(fileVariable), n = numberOfRows)
 
 #pc.fit <- pc(suffStat, indepTest = gaussCItest, alpha = 0.01, labels = labelName, verbose = TRUE)
 
-skel.fit <- skeleton(suffStat, indepTest = gaussCItest, p = ncol(fileVariable[3:50]), labels = labelName, alpha = 0.01)
+skel.fit <- skeleton(suffStat, indepTest = gaussCItest, p = ncol(fileVariable), labels = labelName, alpha = 0.01)
 #Print Adjacency Matrix
-print(as(skel.fit, "amat"))
+adjmat <- as(skel.fit, "amat")
+n.states = 27
+mrf.new <- make.crf(adjmat, n.states)
+
+mrf.new <- make.features(mrf.new)
+mrf.new <- make.par(mrf.new, 4)
+print(mrf.new$node.par)
+
+#mrf.new <- make.par(mrf.new, 4)
+mrf.new$node.par[1,1,1] <- 1
+for (i in 1:mrf.new$n.edges)
+{
+  mrf.new$edge.par[[i]][1,1,1] <- 2
+  mrf.new$edge.par[[i]][1,2,1] <- 3
+  mrf.new$edge.par[[i]][2,1,1] <- 4
+}
+mrf.new <- train.mrf(mrf.new, fileVariable)
 #if (require(Rgraphviz)) {
   #The method below is just to split plot in 1,1 or 1,2 to print
   par(mfrow=c(1,1))
