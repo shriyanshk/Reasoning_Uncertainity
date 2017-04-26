8e8a984d2d30a5f4e0329d5c13646ac140ef243c
Shriyansh Kothari <shriyanshk@yahoo.com>
2017-04-25 18:04:47 -0600

    Update bayesianNetwork.r

--- bayesianNetwork.r
+++ bayesianNetwork.r
@@ -10,17 +10,15 @@ library(igraph)
 library(bnlearn)
 
 
-all_content = readLines("C:/Users/shriy/Downloads/LIDARNoMissingAfterDiscretized.csv")
-#skip_second = all_content[-1]
-fileVariable <- read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
+fileVariable <- read.csv(file = "C:/Users/shriy/Downloads/LIDARNoMissingBeforeDiscretized.csv", header = T ,colClasses = c(rep('factor',24)))
 
-numberOfRows <- nrow(fileVariable) 
-labelName <- colnames(fileVariable) 
-x <- as.data.frame(fileVariable)
+#numberOfRows <- nrow(fileVariable) 
+#labelName <- colnames(fileVariable) 
+#x <- as.data.frame(fileVariable)
 #This learning.test works if inputed instead of x in gs algorithm
 #print(learning.test)
 
-bn.gs <- gs(x, cluster = NULL, whitelist = NULL, blacklist = NULL, test = NULL, alpha = 0.05, B = NULL, debug = FALSE, optimized = TRUE, strict = FALSE, undirected = FALSE)
+bn.gs <- gs(fileVariable, cluster = NULL, whitelist = NULL, blacklist = NULL, test = NULL, alpha = 0.05, B = NULL, debug = FALSE, optimized = TRUE, strict = FALSE, undirected = FALSE)
 
 #Hill Climbing
 #bn.gs <- hc(fileVariable, start = NULL, whitelist = NULL, blacklist = NULL, score = NULL, debug = FALSE, restart = 0, perturb = 1, max.iter = Inf, maxp = Inf, optimized = TRUE)
@@ -29,6 +27,6 @@ adjmat <- amat(bn.gs)
 g2 <- graph_from_adjacency_matrix(adjmat)
 plot(g2)
 bn.plot <- moralizeMAT(adjmat)
-print(bn.plot)
+#print(bn.plot)
 g1 <- graph_from_adjacency_matrix(bn.plot)
 plot(g1)
